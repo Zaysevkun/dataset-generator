feecd498aed3c5e35fd96cad8a3dc0bd2cf68708
zaysevkun <zaysev100@gmail.com>
2021-04-25 18:27:05 +1000

    added models and added .idea to gitignore

--- dataset_generator/models.py
+++ dataset_generator/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class DataType(models.Model):
+    name = models.CharField(max_length=32)
+    data_type = models.CharField(max_length=32)
+
+
+class Schema(models.Model):
+    class SeparatorChoices(models.TextChoices):
+        COMMA = ',', 'comma(,)'
+        SEMICOLON = ';', 'semicolon(;)'
+
+    class StringCharacterChoices(models.TextChoices):
+        DOUBLEQUOTE = '"', 'double-quote(")'
+        QUOTE = '\'', 'quote(\')'
+
+    name = models.CharField(max_length=32)
+    separator = models.CharField(max_length=1, choices=SeparatorChoices.choices, default=SeparatorChoices.COMMA)
+    string_character = models.CharField(
+        max_length=1,
+        choices=StringCharacterChoices.choices,
+        default=StringCharacterChoices.DOUBLEQUOTE
+    )
+    modified = models.DateField(auto_now=True)
+    fields = models.ManyToManyField(DataType, through='Field')
+
+
+class Field(models.Model):
+    schema = models.ForeignKey(Schema, models.CASCADE)
+    datatype = models.ForeignKey(DataType, models.CASCADE)
+    order = models.IntegerField(max_length=64)
+
+
+class Dataset(models.Model):
+    class StatusChoices(models.TextChoices):
+        PROCESSING = 'processing'
+        READY = 'ready'
+
+    schema = models.ForeignKey(Schema, models.CASCADE)
+    status = models.CharField(max_length=11, choices=StatusChoices.choices, default=StatusChoices.PROCESSING)
+    file = models.FileField(upload_to='storage')
